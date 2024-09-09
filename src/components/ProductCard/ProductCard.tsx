df715b4ae3e2bc13fe75939fc28f7d02fce5d40d
Danil-Golubev <dokers190rus@mail.ru>
2024-09-09 10:19:34 +0300

    made mainpage sample

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,27 @@
+import { Button, Select } from '@mui/material';
+import styles from './styles.module.css';
+
+export const ProductCard = () => {
+	return (
+		<>
+			<div className={styles.mainBlock}>
+				<div className={styles.imageBlock}>
+					<img
+						alt='123'
+						src='https://www.homer.com/cdn/shop/products/220901_SphereLegs_Leaf_01__2048_800x.png?v=1704301791'
+					/>
+				</div>
+				<div className={styles.infoBlock}>
+					<div className={styles.title}>Sphere Legs Pendant</div>
+					<div className={styles.desc}>Cody Green</div>
+					<div className={styles.variants}></div>
+					<Select className={styles.select} />
+					<Select className={styles.select} />
+					<div className={styles.addButton}>
+						<div className={styles.buttonTitle}>Add To Cart</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,52 @@
+import { ProductCard } from '../../components/ProductCard/ProductCard';
+import styles from './styles.module.css';
+
+export const MainPage = () => {
+	return (
+		<>
+			<div className={styles.main}>
+				<img
+					alt='Main-image'
+					className={styles.mainImage}
+					src='https://www.homer.com/cdn/shop/files/desktop_45547f03-5bbe-4ace-9fc5-832e459000c0_1800x.png?v=1687880815'
+				/>
+				<div className={styles.showcaseBlock}>
+					<ProductCard />
+					<ProductCard />
+					<ProductCard />
+					<ProductCard />
+					<ProductCard />
+					<ProductCard />
+					<ProductCard />
+					<ProductCard />
+					<ProductCard />
+				</div>
+				<div
+					aria-hidden='true'
+					className='section-catalog-background section-catalog-background--video'
+					role='presentation'
+				>
+					<div className={styles.videoBlock}>
+						<div className={styles.magazineBlock}>
+							<img src='https://www.homer.com/cdn/shop/files/0_front-cover_1200x.png?v=1684768508' />
+						</div>
+
+						<div className={styles.storeInfoBlock}></div>
+						<video className={styles.video} autoPlay={true} loop={true} muted={true}>
+							<source
+								src='https://cdn.shopify.com/videos/c/o/v/3336e4477eba43ecaf88e6023908e5b3.mp4'
+								type='video/mp4'
+							/>
+						</video>
+					</div>
+					<div className={styles.storeInfo}>
+						<img
+							className={styles.storeInfoImage}
+							src='https://www.homer.com/cdn/shop/files/DSCF9361_rafv2_2000px_2000x.jpg?v=1668092669'
+						/>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
